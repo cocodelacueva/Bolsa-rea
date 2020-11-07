0a5b4d356a8971e09fad6ed3d7740302bf39e819
coco <coco@lacueva.tv>
2020-11-07 00:02:52 -0300

    tengo la base del grafico, falta traducirlo al home

--- src/components/simbolo-grafico.jsx
+++ src/components/simbolo-grafico.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { DataContext } from '../context/DataProvider';
 import { Line } from 'react-chartjs-2';
 
 const SimboloGrafico = (props) => {
-    console.log(props.simbolo)
+    
+    //contexto
+    const { fetchData } = React.useContext(DataContext);
 
     const [dataGrafico, setdataGrafico] = React.useState({});
     React.useEffect(() => {
-        //data set mentiroso
+        
+        initGrafico();
+
+    }, [])
+
+
+    const initGrafico = async () => {
+        const datosGrafico = await makeDataGrafico(props.simbolo, props.panel);
+        
         const data = {
-            labels: ['10-10-2020', '11-10-2020', '12-10-2020', '13-10-2020', '14-10-2020', '15-10-2020', '16-10-2020'],
+            labels: datosGrafico.labels.reverse(),
             datasets: [
                 {
-                    label: props.simbolo,
+                    label: props.simbolo.simbolo,
                     fill: false,
                     lineTension: 0.1,
                     backgroundColor: 'rgba(75,192,192,0.4)',
@@ -29,14 +40,72 @@ const SimboloGrafico = (props) => {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: ['54', '54.15', '54.150', '54.201', '54.200', '56', '54.300']
+                    data: datosGrafico.valores.reverse()
                 }
             ]
           };
-        setdataGrafico(data)
-    }, [])
+
+          setdataGrafico(data)
+    }
 
 
+    //busca los datos en el local host o si es necesario hace un push a la bd
+    const makeDataGrafico = async (item, tipo, daysbefore=31) => {
+        let datosNull = 0;//cuenta cuantas veces devuelve null el fetch, si recibe mas de 4 veces null, no vale la pena seguir con la funcion, significa que no hay mas data
+        let data = {}
+        data.labels = []
+        data.valores = []
+
+        //define fecha actual
+        const ultimaFecha = new Date(item.fechaData);
+        for (let i = 0; i < daysbefore; i++) {
+          
+            const fechaBase = ultimaFecha
+            const milisegundos = 1000*60*60*24*i;
+            const fechaloop = fechaBase.getTime() - milisegundos;
+
+            let datos = localStorage.getItem(tipo+"_"+(new Date(fechaloop).toJSON().slice(0, 10)));
+
+            if ( datos ) {
+                datos = JSON.parse(datos);
+                datos = datos.value;
+                
+                const datoAObtener = datos.titulos.filter(titulo => titulo.simbolo === item.simbolo);
+                
+                data.labels.push(new Date(datoAObtener[0].fechaData).toJSON().slice(0, 10));
+                data.valores.push(datoAObtener[0].ultimoCierre);
+            } else {
+                const dataDB = await fetchData(false, tipo, ["date", "==", new Date(fechaloop).toJSON().slice(0, 10)]);
+                
+                //si devuelve data guardamos en localhost y devolvemos
+                if (dataDB) {
+                    //guardamos en localhost siempre con la fecha del registro de firebase
+                    const itemtosave = {
+                        value:  dataDB[0]
+                    }
+                    //guardamos en local store para que quede el registro
+                    localStorage.setItem(tipo+"_"+(new Date(fechaloop).toJSON().slice(0, 10)), JSON.stringify(itemtosave));
+
+                    //completamos la info para el grafico
+                    const datoAObtener = dataDB[0].titulos.filter(titulo => titulo.simbolo === item.simbolo);
+                    
+                    data.labels.push(new Date(datoAObtener[0].fechaData).toJSON().slice(0, 10))
+                    data.valores.push(datoAObtener[0].ultimoCierre)
+
+                } else {
+                    //si devuelve null sumamos un null y luego chequeamos, si son mas de 4 nulls rompemos el for
+                    datosNull++;
+
+                    if (datosNull > 4) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return data;
+
+    }
 
     return (
         <div>
--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -96,73 +96,55 @@ const DataProvider = (props) => {
     //por defecto hay que pasar la coleccion a buscar y la fecha
     // con estas variables se guarda en localhost
     // sino esta en local host hace un fetch a firebase desde el ultimo
-    const obtenerArrayData = async ( coleccion, fecha, estado ) => {
-        
+    const obtenerArrayData = async ( coleccion, fecha ) => {
         //busca en localhost
         let oldData = localStorage.getItem(coleccion+'_'+fecha);
 
         if ( oldData ) {
             //chequeamos si la fecha es hoy, chequeamos que la info no es mayor a 30 minutos y si lo es hacemos un nuevo fetch
-
             oldData = JSON.parse(oldData)
-            const now = new Date();
-            // compare the expiry time of the item with the current time
-            if (now.toJSON().slice(0, 10) !== fecha && now.getTime() > oldData.expiry) {
-                console.log('es muy viejo para ser de la misma fecha');
-                // If the item is expired, delete the item from storage
-                localStorage.removeItem(coleccion+'_'+fecha);
-                // and fetchdata
-                const dataDB = await fetchData(true, coleccion, fecha);
-                console.log(dataDB)
-
-                //guardamos en localhost
-                const now = new Date();
-                const item = {
-                    value: dataDB[0],
-                    expiry: now.getTime() + 1000*60*30,
-                }
-                localStorage.setItem(coleccion+'_'+(new Date().toJSON().slice(0, 10)), JSON.stringify(item));
-
-                //seteamos los estados
-                if (estado === 'cotizaciones') {
-                    debugger;
-                    setCotizaciones(dataDB[0]);
-                } else if ( estado === 'dolares' ) {
-                    setDolares(dataDB[0]);
-                } else if ( estado === 'monedas_digitales' ) {
-                    setmonedasDigitales(dataDB[0])
-                }
-            } else {
-                //si esta fresca retornamos estado:
-                if (estado === 'cotizaciones') {
-                    setCotizaciones(oldData.value);
-                } else if ( estado === 'dolares' ) {
-                    setDolares(oldData.value);
-                } else if ( estado === 'monedas_digitales' ) {
-                    setmonedasDigitales(oldData.value)
-                }
+            //si esta fresca retornamos estado:
+            if (coleccion === 'panel_general' || coleccion === 'panel_cedears') {
+                setCotizaciones(oldData.value);
+            } else if ( coleccion === 'cotizacion_dolares' ) {
+                setDolares(oldData.value);
+            } else if ( coleccion === 'cotizacion_monedas_digitales' ) {
+                setmonedasDigitales(oldData.value)
             }
 
         } else {
             //no esta en localhost, obtenemos la data
-            const dataDB = await fetchData(true, coleccion);
+            const dataDB = await fetchData(false, coleccion, ["date", "==", fecha]);
+            
+            //si devuelve data guardamos en localhost y devolvemos
+            if (dataDB) {
+                //guardamos en localhost siempre con la fecha del registro de firebase
+                const item = {
+                    value:  dataDB[0]
+                }
+                localStorage.setItem(coleccion+'_'+dataDB[0].date, JSON.stringify(item));
 
-            //guardamos en localhost
-            const now = new Date();
-            const item = {
-                value:  dataDB[0],
-                expiry: now.getTime() + 1000*60*30,
+                //seteamos los estados
+                if (coleccion === 'panel_general' || coleccion === 'panel_cedears') {
+                    setCotizaciones(dataDB[0]);
+                } else if ( coleccion === 'cotizacion_dolares' ) {
+                    setDolares(dataDB[0]);
+                } else if ( coleccion === 'cotizacion_monedas_digitales' ) {
+                    setmonedasDigitales(dataDB[0])
+                }
+            } else {
+                //si no hay data recuperamos la ultima pero no la guardamos en localhost
+                const dataDB = await fetchData(true, coleccion);
+                //seteamos los estados
+                if (coleccion === 'panel_general' || coleccion === 'panel_cedears') {
+                    setCotizaciones(dataDB[0]);
+                } else if ( coleccion === 'cotizacion_dolares' ) {
+                    setDolares(dataDB[0]);
+                } else if ( coleccion === 'cotizacion_monedas_digitales' ) {
+                    setmonedasDigitales(dataDB[0])
+                }
             }
-            localStorage.setItem(coleccion+'_'+(new Date().toJSON().slice(0, 10)), JSON.stringify(item));
-
-            //seteamos los estados
-            if (estado === 'cotizaciones') {
-                setCotizaciones(dataDB[0]);
-            } else if ( estado === 'dolares' ) {
-                setDolares(dataDB[0]);
-            } else if ( estado === 'monedas_digitales' ) {
-                setmonedasDigitales(dataDB[0])
-            }
+            
         }
     
     }
@@ -171,16 +153,38 @@ const DataProvider = (props) => {
         setpanelSelec(panel);
     }
 
-    const fetchData = async ( multiple=false, coleccion, where=false, limit=1, ) => {
+
+
+    /*
+    * fectch a firestore
+    */
+    const fetchData = async ( lastitem=false, coleccion, where=null, limit=1, ) => {
        
         try {  
-            if (multiple) {
-                const data = await db.collection(coleccion).orderBy("date", "desc").limit(limit).get();        
-                const arrayData = data.docs.map(doc => ( doc.data() ));
-                return arrayData;
+            if (lastitem) {
+                const data = await db.collection(coleccion).orderBy("date", "desc").limit(limit).get();
+                if (data.docs.length > 0) {
+                    const arrayData = data.docs.map(doc => ( doc.data() ));
+                    return arrayData;
+                } else {
+                    return null;
+                }
+               
             } else {
-                //si la busqueda no es multiple, busca algo específico, debe definir where
-
+                //si la busqueda no es por el utlimo, busca algo específico, debe definir where
+                if (where != null) {
+                    const data = await db.collection(coleccion).where(where[0], where[1], where[2]).limit(limit).get(); 
+                   
+                    if (data.docs.length > 0) {
+                        const arrayData = data.docs.map(doc => ( doc.data() ));
+                        return arrayData;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    console.error("falta definir where param")
+                }
+                
             }
             
     
@@ -192,7 +196,7 @@ const DataProvider = (props) => {
 
     return (
         <DataContext.Provider value={{
-            usuario, iniciarSesion, cerrarSesion, modificarPerfilUsuario, updatePreferenciaUsuario, panelSelec, definePanelAcciones, obtenerArrayData, cotizaciones, monedasDigitales, dolares
+            usuario, iniciarSesion, cerrarSesion, modificarPerfilUsuario, updatePreferenciaUsuario, panelSelec, definePanelAcciones, obtenerArrayData, fetchData, cotizaciones, monedasDigitales, dolares
         }}>
            {props.children} 
         </DataContext.Provider>
